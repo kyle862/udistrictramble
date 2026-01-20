d1741eb5f6f7d5f9d6aa6fffc3aaac4541d0896c
kylewendt <kyle@kylewendt.com>
2026-01-19 18:47:49 -0800

    Layout styles, About page content updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="flex relative ml-56 text-stone-900">
         <NavBar />
-        <div className="py-4 px-12 bg-stone-600">{children}</div>
+        <div className="py-4 px-12 min-w-full min-h-dvh bg-stone-600">
+          {children}
+        </div>
       </body>
     </html>
   );
