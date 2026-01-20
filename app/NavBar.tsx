4af8f17136c64053819cd3f70dea6b7f60d76ea7
kylewendt <kyle@kylewendt.com>
2026-01-19 17:50:18 -0800

    Updated NavBar component with relative link paths

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,10 +3,10 @@ export default function NavBar() {
     <nav className="pt-14 px-4 h-dvh bg-stone-500 min-w-56 fixed left-0 top-0 bottom-0">
       <ul className="flex flex-col gap-4">
         <li className="flex-1">
-          <a href="/">University District Ramble</a>
+          <a href="./">University District Ramble</a>
         </li>
         <li className="flex-1">
-          <a href="/about">About</a>
+          <a href="./about">About</a>
         </li>
       </ul>
     </nav>
